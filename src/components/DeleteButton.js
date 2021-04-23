118001d2e93e8eafbee88540ddddf8709bf0b99b
TwoSavage <twosavage@pop-os.localdomain>
2021-04-23 11:40:22 +0300

    Client Side

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,70 @@
+import React , {useState} from 'react'
+import gql from 'graphql-tag';
+import {useMutation} from '@apollo/react-hooks';
+import {FETCH_POSTS_QUERY} from '../util/graphql';
+import {Button ,Icon , Confirm,Popup } from 'semantic-ui-react';
+function DeleteButton({postId,commentId,callback}) {
+    const [confirmOpen,setConfirmOpen] = useState(false);
+    const mutation = commentId? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+    const [deletePost] = useMutation(mutation,{
+        update(proxy){
+            setConfirmOpen(false);
+            //Remove Post from cache
+           if(!commentId){
+            const data = proxy.readQuery({
+                query : FETCH_POSTS_QUERY
+            });
+           const NewgetPosts= data.getPosts.filter(p=>p.id !== postId)
+            proxy.writeQuery({
+                query : FETCH_POSTS_QUERY,data:{
+                    getPosts:NewgetPosts
+                }});
+           }
+            
+            if(callback) callback();
+        },
+        variables: {
+            postId,
+            commentId
+        }
+    })
+    return (
+        <>
+       <Popup content ={commentId ? "Delete Comment" : "Delete Post"} inverted 
+       trigger={
+        <Button as="div" color="red" floated = "right" onClick={()=>setConfirmOpen(true)}>
+        <Icon name="trash" style={{margin:0}}/>
+      </Button>
+       }
+       />
+      <Confirm 
+       open={confirmOpen}
+       onCancel={() => setConfirmOpen(false)}
+       onConfirm={deletePost}
+       />
+       </>
+    )
+}
+
+const DELETE_POST_MUTATION = gql`
+mutation deletePost($postId: ID!){
+    deletePost(postId:$postId)
+}
+`;
+
+const DELETE_COMMENT_MUTATION = gql`
+    mutation deleteComment($postId: ID!, $commentId: ID!){
+        deleteComment(postId: $postId, commentId:$commentId){
+            id
+            comments{
+                id
+                username
+                createdAt
+                body
+            }
+            commentCount
+        }
+    }
+`;
+
+export default DeleteButton
