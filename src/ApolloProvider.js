9362065868f0004d6295159a6ebfda40ff7a2d44
TwoSavage <twosavage@pop-os.localdomain>
2021-04-24 09:31:12 +0300

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from '@apollo/react-hooks';
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5001'
+    uri: 'https://iconnectv2.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
